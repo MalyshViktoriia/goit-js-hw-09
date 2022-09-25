632fe8bb95e5e031948ea5c237b38b676a88228b
Viktoriia Malysh <pylypenko.viktoriia@gmail.com>
2022-09-25 11:37:04 +0200

    add the first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
 
+let randomId = null;
+
+refs.start.addEventListener('click', changeTheme);
+refs.stop.addEventListener('click', onStopHandle);
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeTheme() {
+
+    randomId = setInterval(() => { refs.body.style.backgroundColor = getRandomHexColor() }, 1000);
+    refs.start.disabled = true;
+};
+
+function onStopHandle() {
+    clearInterval(randomId);
+    refs.start.disabled = false;
+};
